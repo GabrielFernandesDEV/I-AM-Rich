4d1f06612a6ef23a0598078cb9026b0c523d012c
Gabriel Fernandes <49999537+GabrielFernandesDEV@users.noreply.github.com>
2021-04-07 10:07:39 -0300

    Add files via upload

---
+++ I AM Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM Rich
+//
+//  Created by Gabriel on 26/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
